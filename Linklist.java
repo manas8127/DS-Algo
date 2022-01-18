2e6d6ac433e9c4eec2bf5b2c8d70812ba87ef9cd
manas8127 <contact2manas24@gmail.com>
2022-01-18 22:00:22 +0530

    Linklist

---
+++ Linklist.java
@@ -0,0 +1,38 @@
+public class Linklist
+{
+    Node head;
+    static class Node{
+    
+        int value;
+    
+        Node next;
+    
+        Node(int d)
+        {
+            value=d;
+            next=null;
+        }
+    
+    }
+    public static void main(String sd[]){
+        Linklist list=new Linklist();
+
+        list.head=new Node(1);
+        Node second=new Node(2);
+        Node third=new Node(3);
+        
+        list.head.next=second;
+        second.next=third;
+
+        while(list.head!=null)
+        {
+            System.out.println(list.head.value);
+            list.head=list.head.next;
+        }
+
+    }
+
+
+    
+
+}
